abe2fca28caae6132117cc22cbc13c16116bc17a
priyanka.byahatti <priyanka.byahatti@momox.biz>
2024-12-14 02:44:30 +0530

    - added post request

--- queries/deals.sql
+++ queries/deals.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS deals CASCADE;
 CREATE TABLE deals (
     deal_id bigserial NOT NULL,
     deal_name varchar(50) NOT NULL,
-    amount float,
+    deal_size float,
     probability_of_closure float,
     deal_Stage varchar(30),
     PRIMARY KEY (deal_id),
